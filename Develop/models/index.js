26303499f0087028e43794f3424566bbd7d22bc6
Donald Biggers <DuG@tests-MacBook-Air.local>
2022-01-15 18:56:11 -0600

    created models functionality

--- Develop/models/index.js
+++ Develop/models/index.js
@@ -1,16 +1,31 @@
 // import models
-const Product = require('./Product');
-const Category = require('./Category');
-const Tag = require('./Tag');
-const ProductTag = require('./ProductTag');
+const Product = require("./Product");
+const Category = require("./Category");
+const Tag = require("./Tag");
+const ProductTag = require("./ProductTag");
 
-// Products belongsTo Category
+Product.belongsTo(Category, {
+  foreignKey: "category_id",
+  onDelete: "CASCADE",
+});
 
-// Categories have many Products
+Category.hasMany(Product, {
+  foreignKey: "category_id",
+});
 
-// Products belongToMany Tags (through ProductTag)
+Product.belongsToMany(Tag, {
+  through: ProductTag,
+  as: "productTag_id",
+  foreignKey: "product_id",
+  onDelete: "SET NULL",
+});
 
-// Tags belongToMany Products (through ProductTag)
+Tag.belongsToMany(Product, {
+  through: ProductTag,
+  as: "productTag_id",
+  foreignKey: "tag_id",
+  onDelete: "SET NULL",
+});
 
 module.exports = {
   Product,
